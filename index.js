44078d0e495c667754e74d506a4c25282046efa9
penneytech <99091373+penneytech@users.noreply.github.com>
2024-11-25 11:21:56 -0500

    First commit.

---
+++ index.js
@@ -0,0 +1,9 @@
+/* INSTRUCTIONS:
+
+1) Create an array containing 5 words (Tree species? Candy bars?)
+2) Use a While loop to print each word to the console.
+3) End the program by writing "Thats all!" to the console. 
+
+HINT: 'arrayname.length' will return the length of the number of elements in an array.
+
+*/
